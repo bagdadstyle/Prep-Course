598c7aa4a807a8fe98245da89d4d606a8899284b
bagdadstyle <mau20410@gmail.com>
2022-06-19 18:58:29 -0300

    patch3homeWork

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -137,9 +137,9 @@ function operadoresLogicos(num1, num2, num3) {
       return 'Error'}
   if (num1 < 0 || num2 < 0 || num3 < 0){
       return 'Hay negativos'}
-   if (num1 > num2 || num1 > num3){
+   if (num1 > num2 & num1 > num3){
       return 'Número 1 es mayor y positivo'}
-  if (num3 > num1 || num3 > num2){
+  if (num3 > num1 & num3 > num2){
       return num3+1}
   else{
       return false}
@@ -152,14 +152,28 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero < 2){
+    return false;
+  }
+  for (let i=2; i < numero; i++){
+    if (numero % i === 0)
+      return false;
+  }
+  return true;
+
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if (valor === false){
+    return'Soy falso'
+  }
+  if (valor === true){
+    return 'Soy verdadero'}
+  }
 
-}
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
