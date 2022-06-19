6e84a4ae3b9cb664b91f75e76d668aa3c97a503e
bagdadstyle <mau20410@gmail.com>
2022-06-19 13:08:09 -0300

    patchHomework2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -63,18 +63,38 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch(color){
+    case 'red':
+      return `This is ${color}`
+    case 'blue':
+      return `This is ${color}`
+    case 'green':
+      return `This is ${color}`
+    case 'orange':
+      return `This is ${color}`
+    break;
+      default:
+      return 'Color not found'
+  }
+
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if ( numero == 10 || numero == 5){
+      return true}
+      return false
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50 && numero > 20){
+      return true}
+      return false
 }
 
 function esEntero(numero) {
@@ -85,6 +105,10 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero == Math.floor(numero)){
+      return true
+  }
+      return false;
 }
 
 function fizzBuzz(numero) {
@@ -92,6 +116,14 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 5 == 0 && numero % 3 == 0){
+    return 'fizzbuzz'}
+  else if(numero % 5 == 0){
+    return 'buzz'}
+  else if(numero % 3 == 0){
+    return 'fizz'}
+  return numero;
+  
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -101,6 +133,18 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if ( num1 == 0 || num2 == 0 || num3 == 0){
+      return 'Error'}
+  else if (num1 < 0 || num2 < 0 || num3 < 0){
+      return 'Hay negativos'}
+  else if (num1 > num2 || num1 > num3){
+      return 'Número 1 es mayor y positivo'}
+  else if (num3 > num1 || num3 > num2){
+      return num3+1}
+  else{
+      return false
+  }
+
 }
 
 function esPrimo(numero) {
