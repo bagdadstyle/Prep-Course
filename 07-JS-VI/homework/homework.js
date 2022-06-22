fa7a301f3585913bb1870960c5cdb225b0fe2f20
bagdadstyle <mau20410@gmail.com>
2022-06-22 20:52:56 -0300

    homeWorkVI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -46,6 +46,10 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var arreglo = array.map(function(num){
+    return cb(num);
+  })
+  return arreglo;
 }
 
 function filter(array) {
